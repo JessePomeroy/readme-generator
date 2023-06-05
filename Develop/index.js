bb0b45bf8f94aa6a53d15be16bb1153e95133f9f
JessePomeroy <thinkingofview@gmail.com>
2023-06-05 16:08:25 -0500

    tweaks

--- Develop/index.js
+++ Develop/index.js
@@ -2,73 +2,73 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const path = require('path'); // using path to join directory names to form a full path
-const genReadme = require('./genReadme.js'); // using the genReadme.js file to generate the README.md file
+const generateMarkdown = require('./utils/generateMarkdown.js'); // using the genReadme.js file to generate the README.md file
 
 // TODO: Create an array of questions for user input
 const questions = [
-    {
-      type: 'input',
-      name: 'title',
-      message: 'please provide a title for your project.',
-      validate: (value) => {if(value){return true} else {return `please do not leave blank.`}}
-    },
-    {
-      type: 'input',
-      name: 'link',
-      message: 'please provide a link to your project.',
-      validate: (value) => {if(value){return true} else {return `please do not leave blank.`}}
+  {
+    type: 'input',
+    name: 'title',
+    message: 'please provide a title for your project.',
+    validate: (value) => { if (value) { return true } else { return `please do not leave blank.` } }
+  },
+  {
+    type: 'input',
+    name: 'link',
+    message: 'please provide a link to your project.',
+    validate: (value) => { if (value) { return true } else { return `please do not leave blank.` } }
 
-    },
-    {
-      type: 'input',
-      name: 'description',
-      message: 'please provide a short description of your project.',
-      validate: (value) => {if(value){return true} else {return `please do not leave blank.`}}
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'please provide a short description of your project.',
+    validate: (value) => { if (value) { return true } else { return `please do not leave blank.` } }
 
-    },
-    {
-      type: 'checkbox',
-      name: 'license',
-      message: 'please check any and all licenses that apply to your project.',
-      choices: ['mit', 'cc', 'gpl', 'afl-3.0', 'artistic-2.0'],
-      validate: (value) => {if(value){return true} else {return `please do not leave blank.`}}
+  },
+  {
+    type: 'checkbox',
+    name: 'license',
+    message: 'please check any and all licenses that apply to your project.',
+    choices: ['mit', 'cc', 'gpl', 'afl-3.0', 'artistic-2.0'],
+    validate: (value) => { if (value) { return true } else { return `please do not leave blank.` } }
 
-    },
-    {
-      type: 'input',
-      name: 'features',
-      message: 'tell us a little bit about what your project can do.',
-      validate: (value) => {if(value){return true} else {return `please do not leave blank.`}}
+  },
+  {
+    type: 'input',
+    name: 'features',
+    message: 'tell us a little bit about what your project can do.',
+    validate: (value) => { if (value) { return true } else { return `please do not leave blank.` } }
 
-    },
-    {
-      type: 'input',
-      name: 'dependencies',
-      message: 'list any dependencies your project requires here.',
-      validate: (value) => {if(value){return true} else {return `please do not leave blank.`}}
+  },
+  {
+    type: 'input',
+    name: 'dependencies',
+    message: 'list any dependencies your project requires here.',
+    validate: (value) => { if (value) { return true } else { return `please do not leave blank.` } }
 
-    },
-    {
-      type: 'input',
-      name: 'usage',
-      message: 'please provide the languages and technologies used in this project.',
-      validate: (value) => {if(value){return true} else {return `please do not leave blank.`}}
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'please provide the languages and technologies used in this project.',
+    validate: (value) => { if (value) { return true } else { return `please do not leave blank.` } }
 
-    },
-    {
-      type: 'input',
-      name: 'email',
-      message: 'please provide your email you want associated with this project.',
-      validate: (value) => {if(value){return true} else {return `please do not leave blank.`}}
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'please provide your email you want associated with this project.',
+    validate: (value) => { if (value) { return true } else { return `please do not leave blank.` } }
 
-    },
-    {
-      type: 'input',
-      name: 'credit',
-      message: 'please credit the contributors to this project.',
-      validate: (value) => {if(value){return true} else {return `please do not leave blank.`}}
+  },
+  {
+    type: 'input',
+    name: 'credit',
+    message: 'please credit the contributors to this project.',
+    validate: (value) => { if (value) { return true } else { return `please do not leave blank.` } }
 
-    },
+  },
 ];
 
 // TODO: Create a function to write README file
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,59 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function genLicenseBadge(license) {
+  if (license !== 'none') {
+    return `![Github license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  }
+  return "";
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+# ${data.title}
+${genLicenseBadge(data.license)}
+## Description
+${data.description}
+## Project Deployment
+${data.link}
+## Table of Contents
+* [Features](#features)
+* [Dependencies](#dependencies)
+* [Usage](#usage)
+* [Credits](#credits)
+* [Contact](#contact)
+## Features
+${data.features}
+## Dependencies
+* ${data.dependencies}
+## Usage
+${data.usage}
+## Credits
+${data.credits}
+## Contact
+If you have any questions or comments or would like to contribute to
+the project please contact me [here](mailto:${data.email}?subject=[GitHub]%20Dev%20Connect)
 
 `;
 }
 
 module.exports = generateMarkdown;
+
+// // TODO: Create a function that returns a license badge based on which license is passed in
+// // If there is no license, return an empty string
+// function renderLicenseBadge(license) {}
+
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink(license) {}
+
+// // TODO: Create a function that returns the license section of README
+// // If there is no license, return an empty string
+// function renderLicenseSection(license) {}
+
+// // TODO: Create a function to generate markdown for README
+// function generateMarkdown(data) {
+//   return `# ${data.title}
+
+// `;
+// }
+
+// module.exports = generateMarkdown;
+
